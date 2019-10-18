6c2caf2020aa1a1dd728a2ea552b5feef4871f07
Silvio Peroni <essepuntato@gmail.com>
2019-10-18 15:37:04 +0200

    open in writing the entity

--- test/test.py
+++ test/test.py
@@ -3,7 +3,7 @@ from re import sub
 existing_doi = set()
 
 with open("/srv/data/glob/id_date.csv") as f:
-    with open("/srv/data/glob/id_date_nodup.csv") as g:
+    with open("/srv/data/glob/id_date_nodup.csv", "w") as g:
         for r in f.readlines():
             doi = sub('^"(.+)",".*"$', r)
             if doi not in existing_doi:
