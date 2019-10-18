09893e69a734cee872d11076e05e90bc02ccc4ae
Silvio Peroni <essepuntato@gmail.com>
2019-10-18 15:42:10 +0200

    fix bug

--- test/test.py
+++ test/test.py
@@ -5,7 +5,7 @@ existing_doi = set()
 with open("/srv/data/glob/id_date.csv") as f:
     with open("/srv/data/glob/id_date_nodup.csv", "w") as g:
         for r in f.readlines():
-            doi = sub('^"(.+)",".*"$', r)
+            doi = sub('^"(.+)",".*"$', "\1", r)
             if doi not in existing_doi:
                 existing_doi.add(doi)
                 g.write(r)
