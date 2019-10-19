8ed5dbe85d15db19cabdb98d6719bbebb52690bc
Silvio Peroni <essepuntato@gmail.com>
2019-10-19 22:51:21 +0200

    find errors in dates

--- test/test.py
+++ test/test.py
@@ -1,11 +1,17 @@
 from re import sub
+from dateutil.parser import parse
+from datetime import datetime
 
-existing_doi = set()
+DEFAULT_DATE = datetime(1970, 1, 1, 0, 0)
 
 with open("/srv/data/glob/id_date.csv") as f:
-    with open("/srv/data/glob/id_date_nodup.csv", "w") as g:
-        for r in f.readlines():
-            doi = sub('^"(.+)",".*"$', "\\1", r)
-            if doi not in existing_doi:
-                existing_doi.add(doi)
-                g.write(r)
+    is_first = True
+    for r in f.readlines():
+        if is_first:
+            is_first = False
+        else:
+            try:
+                d = sub('^"[^"]+","([^,]*)"$', "\\1", r)
+                citing_pub_datetime = parse(d, default=DEFAULT_DATE)
+            except:
+                print(r)
