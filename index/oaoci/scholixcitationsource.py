75b374220f9eb923e3143988e01b82600d81c573
Giuseppe Grieco <g.grieco1997@gmail.com>
2021-09-28 19:09:55 +0200

    + scholix dump for test

--- index/oaoci/scholixcitationsource.py
+++ index/oaoci/scholixcitationsource.py
@@ -1,8 +1,8 @@
-
 from index.citation.citationsource import CitationSource
 from index.identifier.doimanager import DOIManager
 from csv import DictWriter
 from json import loads
+from index.citation.oci import Citation
 
 from os import walk, sep
 from os.path import isdir, join
@@ -23,6 +23,7 @@ class ScholixCitationSource(CitationSource):
             for name in files:
                 if name.endswith(".scholix"):
                     self.__files.append(join(path, name))
+        self.__files.sort()
                     
     def __open_next_scholix_file(self):
         if not self.__current_file is None:
@@ -44,6 +45,7 @@ class ScholixCitationSource(CitationSource):
         line = self.__current_file.readline()
         self.__last_row += 1
         if line is None or line == "]":
+            self.__current_file.close()
             self.__current_file = None
             return self.__get_next_scholix_item()
 
@@ -72,8 +74,41 @@ class ScholixCitationSource(CitationSource):
     def get_next_citation_data(self):
         item = self.__get_next_scholix_item()
         if item is not None:
-            citing = item["Source"]["Identifier"]["ID"] 
-            cited = item["Target"]["Identifier"]["ID"]
+            citing = item.get("Source")
+            cited = item.get("Target")
+
+            if not citing or not cited:
+                self.update_status_file()
+                return None
+            
+            citing_id = citing.get("Identifier")
+            cited_id = cited.get("Identifier")
+            if not citing_id or not cited_id:
+                self.update_status_file()
+                return None
+
+            citing_doi = citing_id.get("ID")
+            cited_doi = cited_id.get("ID")
+            if not citing_doi or not cited_doi:
+                self.update_status_file()
+                return None
+            
+            citing_doi = self.doi.normalise(citing_doi)
+            cited_doi = self.doi.normalise(cited_doi)
+
+            citing_pubdate = citing.get("PublicationDate")
+            cited_pubdate = cited.get("PublicationDate")
+            
+            timespan = None
+            if not citing_pubdate:
+                citing_pubdate = None
+            else:
+                if not cited_pubdate:
+                    timespan = None
+                else:
+                    c = Citation(None, None, citing_pubdate, None, cited_pubdate, None, None, None, None, "", None, None, None, None, None)
+                    timespan = c.duration
+
             self.update_status_file()
-            return citing, cited, None, None, None
+            return citing_doi, cited_doi, citing_pubdate, timespan, None
         return None
