d356472059519305d3d0db17922cc6bc20571c0d
ivan.heibi <ivanhb.ita@gmail.com>
2023-05-26 10:48:30 +0200

    bugfix: export redis

--- scripts/export_redis_db.py
+++ scripts/export_redis_db.py
@@ -1,5 +1,7 @@
 from redis import Redis
 import argparse
+from tqdm import tqdm
+import csv
 
 parser = argparse.ArgumentParser(description='Export a DB from Redis')
 parser.add_argument('--db', type=str, required=True,help='DB to export')
@@ -11,6 +13,6 @@ rconn_db = Redis(host="localhost", port="6379", db=args.db)
 with open('redis_'+str(args.db)+'.csv', 'a+') as f:
     write = csv.writer(f)
     keys = rconn_db.keys('*')
-    for key in keys:
+    for key in tqdm(keys):
         val = r.get(key).decode('utf-8')
         write.writerow([key.decode('utf-8'),val])
