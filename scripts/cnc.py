dd5ed980a0fc13b8cad97f2696f7f6fb11d0ae4c
Ivan Heibi <ivanhb.ita@gmail.com>
2025-02-18 15:48:44 +0100

    fix omid id bug

--- scripts/cnc.py
+++ scripts/cnc.py
@@ -153,9 +153,9 @@ def normalize_dump(service, input_files, output_dir, newdump = False):
                                     # check if both citing and cited entities have omid
                                     if citing_omid != None and cited_omid != None:
 
-                                        citing_omid = citing_omid.decode("utf-8")
-                                        cited_omid = cited_omid.decode("utf-8")
-                                        oci_omid = citing_omid[3:]+"-"+cited_omid[3:]
+                                        citing_omid = "br/"+citing_omid.decode("utf-8")
+                                        cited_omid = "br/"+cited_omid.decode("utf-8")
+                                        oci_omid = citing_omid+"-"+cited_omid
 
                                         index_ocis[oci_omid] = (citing_omid,cited_omid)
 
