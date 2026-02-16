295c7940f6997454e3e9e5eb5e8cf9a1825de059
Ivan Heibi <ivanhb.ita@gmail.com>
2026-02-16 22:33:50 +0100

    cits2redis fix

--- scripts/cits2redis.py
+++ scripts/cits2redis.py
@@ -1,23 +1,8 @@
 #!python
-# Copyright (c) 2023 Ivan Heibi.
-#
-# Permission to use, copy, modify, and/or distribute this software for any purpose
-# with or without fee is hereby granted, provided that the above copyright notice
-# and this permission notice appear in all copies.
-#
-# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
-# REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND
-# FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT,
-# OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE,
-# DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS
-# ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS
-# SOFTWARE.
-
 import csv
 import json
 from zipfile import ZipFile
 import os
-import io
 import argparse
 from redis import Redis
 import sys
@@ -30,85 +15,118 @@ from oc.index.utils.config import get_config
 _config = get_config()
 _logger = get_logger()
 csv.field_size_limit(sys.maxsize)
+
 rconn = Redis(
     host=_config.get("redis", "host"),
     port=_config.get("redis", "port"),
     db=_config.get("cnc", "db_cits")
 )
 
+# Define what we are searching for in the TTL lines
+NEEDLE = "ci/"
+
+
+def extract_oci_from_line(line):
+    """Extract OCI from a TTL line."""
+    if NEEDLE not in line:
+        return None
+
+    start = line.find("ci/") + 3
+    end = line.find(">", start)
+
+    if start == -1 or end == -1:
+        return None
+
+    return line[start:end]
+
+
 def upload2redis(dump_path="", intype=""):
     """
-    Uploading the citations stored in the RDF data into the Redis dataset
+    Upload citations stored in RDF data into Redis.
 
-    Args:
-        dump_path (string, mandatory): the directory path to the RDF dump
-        intype (string, mandatory): the expected type of files in <dump_path>, i.e. ZIP or TTL
-
-    Returns:
-        ...
+    Redis structure:
+        <cited>: [<citing-1>, <citing-2>, ...]
     """
 
-    # Get all the citations from the Dump: citing and cited entites
-    # Populate the redis DB such that: <cited>: [<citing-1>, <citing-2>, ... <citing-n>]
     index_cited = defaultdict(list)
     all_ocis = []
-    citing_entities_set = set()
+
+    intype = intype.upper()
 
     if intype == "ZIP":
         for filename in os.listdir(dump_path):
             fzip = os.path.join(dump_path, filename)
-            # checking if it is a file
-            if fzip.endswith(".zip"):
-                _logger.info("Reading "+str(fzip)+ " ...")
+
+            if fzip.endswith(".zip") and os.path.isfile(fzip):
+                _logger.info(f"Reading {fzip} ...")
+
                 with ZipFile(fzip) as archive:
-                    for filename in tqdm(archive.namelist()):
-                        if filename.endswith(".ttl"):
-                            with open(filename, "r", encoding="utf-8") as f:
-                                for line in f:
-                                    if needle in line:
-                                        # extract the part between "ci/" and ">"
-                                        start = line.find("ci/") + 3
-                                        end = line.find(">", start)
-                                        oci = line[start:end]
+                    for member in tqdm(archive.namelist()):
+                        if member.endswith(".ttl"):
+                            with archive.open(member) as f:
+                                for raw_line in f:
+                                    line = raw_line.decode("utf-8", errors="ignore")
+                                    oci = extract_oci_from_line(line)
+                                    if oci:
                                         all_ocis.append(oci)
+
     elif intype == "TTL":
         for filename in os.listdir(dump_path):
-            if filename.endswith(".ttl"):
-                with open(filename, "r", encoding="utf-8") as f:
+            fttl = os.path.join(dump_path, filename)
+
+            if fttl.endswith(".ttl") and os.path.isfile(fttl):
+                _logger.info(f"Reading {fttl} ...")
+
+                with open(fttl, "r", encoding="utf-8") as f:
                     for line in f:
-                        if needle in line:
-                            # extract the part between "ci/" and ">"
-                            start = line.find("ci/") + 3
-                            end = line.find(">", start)
-                            oci = line[start:end]
+                        oci = extract_oci_from_line(line)
+                        if oci:
                             all_ocis.append(oci)
+    else:
+        raise ValueError("intype must be either 'ZIP' or 'TTL'")
 
+    # Build citation index
     for oci in all_ocis:
-        citing = oci.split("-")[0]
-        cited = oci.split("-")[1]
-        index_cited[cited].append(citing)
-        citing_entities_set.add(citing)
+        try:
+            citing, cited = oci.split("-", 1)
+            index_cited[cited].append(citing)
+        except ValueError:
+            continue
 
-    _logger.info(f"Storing {len(all_ocis)} citations in Redis (<CITED>:[<CITING-1>, ... <CITING-n>]) ...")
-    rconn.mset( { _k:json.dumps(_v) for _k, _v in index_cited.items() } )
+    _logger.info(
+        f"Storing {len(all_ocis)} citations in Redis "
+        "(<CITED>:[<CITING-1>, ...]) ..."
+    )
+
+    if index_cited:
+        rconn.mset({_k: json.dumps(_v) for _k, _v in index_cited.items()})
 
 
 def main():
-    parser = argparse.ArgumentParser(description='Store the citations of OpenCitations Index in Redis')
+    parser = argparse.ArgumentParser(
+        description="Store the citations of OpenCitations Index in Redis"
+    )
+
     parser.add_argument(
-        '--dump',
+        "--dump",
         type=str,
         required=True,
-        help='The directory containing ZIP files storing the RDF source dump of OpenCitations Index'
+        help="Directory containing TTL or ZIP RDF dump files"
     )
+
     parser.add_argument(
         "-t",
         "--intype",
         required=True,
-        help="The format of the files in the input directory, i.e. TTL or ZIP",
+        help="Input file type: TTL or ZIP",
     )
+
     args = parser.parse_args()
 
     _logger.info("Uploading citations in RDF format to Redis ...")
-    upload2redis(args.dump,args.intype)
+    upload2redis(args.dump, args.intype)
     _logger.info("Done!")
+
+
+if __name__ == "__main__":
+    main()
